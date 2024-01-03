8b4b42a3539b63564d544ca989b293e1be8ff0c4
flybywang <flybywang@tencent.com>
2024-01-03 21:13:24 +0800

    feat: slicefeeder

---
+++ src/feeder/error.zig
@@ -0,0 +1,3 @@
+const FeederError = error{
+    OutOfBound,
+};
---
+++ src/feeder/slice_feeder.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+const err = @import("error.zig");
+const FeederError = err.FeederError;
+const comma = ",";
+
+fn SliceFeeder(comptime T: type) type {
+    return struct {
+        iter: []const T,
+        fn init(slice: []const T) SliceFeeder(T) {
+            return SliceFeeder(T){
+                .iter = slice,
+            };
+        }
+        fn peek(self: SliceFeeder(T)) FeederError!T {
+            if (self.empty()) {
+                return FeederError.OutOfBound;
+            }
+            return self.iter[0];
+        }
+        fn peek_cn(self: SliceFeeder(T), comptime n: usize) FeederError!*const [n]T {
+            if (!self.next_n_in_bound(n)) {
+                return FeederError.OutOfBound;
+            }
+            return self.iter[0..n];
+        }
+        fn peek_n(self: SliceFeeder(T), n: usize) FeederError![]const T {
+            if (!self.next_n_in_bound(n)) {
+                return FeederError.OutOfBound;
+            }
+            return self.iter[0..n];
+        }
+        fn forward_n(self: *SliceFeeder(T), n: usize) FeederError!void {
+            if (!self.next_n_in_bound(n)) {
+                return FeederError.OutOfBound;
+            }
+            self.*.iter = self.iter[n..];
+        }
+        fn empty(self: SliceFeeder(T)) bool {
+            return self.iter.len == 0;
+        }
+        fn next_n_in_bound(self: SliceFeeder(T), n: usize) bool {
+            return n <= self.iter.len;
+        }
+    };
+}
+
+test "peek test" {
+    const sf = SliceFeeder(u8).init(";");
+    try std.testing.expectEqual(try sf.peek(), @as(u8, ';'));
+}
+
+test "peek_cn test" {
+    const sf = SliceFeeder(u8).init("hello");
+    try std.testing.expect(std.mem.eql(u8, try sf.peek_cn(5), "hello"));
+}
+
+test "peek_n test" {
+    const sf = SliceFeeder(u8).init("hello");
+    var n: usize = 5;
+    try std.testing.expect(std.mem.eql(u8, try sf.peek_n(n), "hello"));
+}
+
+test "empty test" {
+    {
+        const sf = SliceFeeder(u8).init("hello");
+        try std.testing.expectEqual(false, sf.empty());
+    }
+    {
+        const sf = SliceFeeder(u8).init("");
+        try std.testing.expectEqual(true, sf.empty());
+    }
+}
+
+test "forward test" {
+    {
+        var sf = SliceFeeder(u8).init("hello");
+        try sf.forward_n(5);
+        try std.testing.expectEqual(true, sf.empty());
+    }
+    {
+        var sf = SliceFeeder(u8).init("hello");
+        try sf.forward_n(2);
+        try std.testing.expect(std.mem.eql(u8, try sf.peek_n(3), "llo"));
+    }
+    {
+        var sf = SliceFeeder(u8).init("hello");
+        try std.testing.expectError(FeederError.OutOfBound, sf.forward_n(6));
+    }
+    {
+        var sf = SliceFeeder(u8).init("");
+        try sf.forward_n(0);
+    }
+}
